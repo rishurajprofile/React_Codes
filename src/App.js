5a96da2138e0460c5b5a37b3584c68119beedff9
Rishu Raj <rrdebug7@gmail.com>
2026-01-22 20:51:52 +0530

    Revised

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Video from './components/Video';
+
+function App() {
+  return (
+    <div>
+      <div>Hello</div>
+      <Video></Video>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Video.js
@@ -0,0 +1,10 @@
+//components are the functions that returns the UI (user interface);
+function Video (){
+  return(
+    <div>
+    <img src="https://images.pexels.com/photos/14082105/pexels-photo-14082105.jpeg">
+    </img>
+    <div> wahi image </div>
+    </div>
+}
+export default Video;
