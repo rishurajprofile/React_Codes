0adaf100e8ffa0c6f282b1867b07135ae7c6b69f
Rishu Raj <rrdebug7@gmail.com>
2026-01-26 13:22:33 +0530

    revised commita

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <div>Hello</div>
-      <Video></Video>
+      <Video bgColor= "green" title = "React Js tutorial" views='10k' time ='1 year ago'channel ='codenation' ></Video>
+      <Video bgColor ="red" title = "Node Js tutorial" views='100k ' time ='10 months ago'channel ='codenation' ></Video>
+      <Video bgColor ="red" title = "mongoDB Js tutorial" views='100k ' time ='10 months ago'channel ='codenation' ></Video>
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,20 @@
+
+import './Video.css'
 //components are the functions that returns the UI (user interface);
-function Video (){
+function Video ({title,bgColor,channel,views,time}){
+  let bg = 'dark'
   return(
-    <div>
-    <img src="https://images.pexels.com/photos/14082105/pexels-photo-14082105.jpeg">
+   <><div className='container'>
+    <div className ="pic">
+    <img src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?semt=ais_hybrid&w=74&q=80" alt='Dummy'>
     </img>
-    <div> wahi image </div>
+   </div>
+    <div className='title'>{title}</div>
+    <div className ='channel'>{channel}</div>
+    <div className='views'>{views}views <span>.</span>{time}</div>
     </div>
+   </> 
+    
+    );
 }
 export default Video;
